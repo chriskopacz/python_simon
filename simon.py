3114ebdf8ba07bc5bf8f1ab7214541a08f535bf8
ck <kopacz.1977@gmail.com>
2017-10-24 20:23:48 -0600

    added run, teardown, simon.py

---
+++ simon.py
@@ -0,0 +1,20 @@
+#Author: Chris Kopacz
+#created: 24 October 2017
+#filename: simon.py
+#play the 4-color, sequence repeating game using the pygame module
+
+import pygame
+from pygame.locals import *
+import time
+
+def main():
+
+    print "Hello\n"
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
